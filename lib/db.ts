7792e7be36801224d1bc877edfc6e39a7d61de0e
Adil Boudida <boudidaadil@gmail.com>
2025-06-07 21:25:58 +0400

    first commit

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,9 @@
 import mongoose from "mongoose";
 
+declare global {
+  var mongoose: any;
+}
+
 const MONGODB_URI = process.env.MONGODB_URI!;
 
 if (!MONGODB_URI) throw new Error("Please define the MONGODB_URI in .env");
@@ -22,3 +26,4 @@ export async function connectDB() {
   cached.conn = await cached.promise;
   return cached.conn;
 }
+
--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -1,8 +1,12 @@
+import type { NextApiRequest, NextApiResponse } from "next";
 import { connectDB } from "@/lib/db";
 import User from "@/models/User";
 import { generateToken } from "@/utils/generateToken";
 
-export default async function handler(req, res) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method !== "POST") return res.status(405).end();
 
   await connectDB();
